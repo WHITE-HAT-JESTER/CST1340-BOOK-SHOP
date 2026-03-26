30021269bbfec2ead53dbf6e529e1c90930f03ef
Courtney <CS1581@live.mdx.ac.uk>
2026-03-26 11:27:38 +0400

    Add JS and CSS for login/signup

--- js/script.js
+++ js/script.js
@@ -20,6 +20,50 @@ theme.addEventListener("click", () => {
  darkmode !== "active" ? enableDarkmode() : disableDarkMode()
 })
 
+// SIGN UP FORM 
+//subject to change as there are naming inconsistencies
+const signup = document.getElementById("signupform");
+const open_sign_up = document.getElementById("sign_up");
+const close_sign_up = document.getElementById("closeSignup");
+const submit_sign_up = document.getElementById("submit_signup");
+
+// opens up modal to sign up
+open_sign_up.addEventListener("click", () => {
+    signup.style.display = "block";
+})
+
+// closes sign up form
+close_sign_up.addEventListener("click", () => {
+    signup.style.display = "none";
+})
+
+// submits form (closes the form as no data is sent anywhere)
+submit_sign_up.addEventListener("click", () => {
+    signup.style.display = "none";
+})
+
+
+// LOG IN FORM
+
+const login = document.getElementById("loginform");
+const open_log_in = document.getElementById("log_in");
+const close_log_in = document.getElementById("closeLogin");
+const submit_log_in = document.getElementById("submit_login");
+
+// opens up modal to log in
+open_log_in.addEventListener("click", () => {
+    login.style.display = "block";
+})
+
+// closes log in form
+close_log_in.addEventListener("click", () => {
+    login.style.display = "none";
+})
+
+// submits form (closes the form as no data is sent anywhere)
+submit_log_in.addEventListener("click", () => {
+    login.style.display = "none";
+})
 
 
 // AFTER MODEL
